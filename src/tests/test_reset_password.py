897d30f890383cf6f6ddcd6cc670f83f58b41b07
saffin <saffin7867@gmail.com>
2024-01-04 22:11:46 +0530

    minor changes

--- src/tests/test_reset_password.py
+++ src/tests/test_reset_password.py
@@ -6,9 +6,7 @@ from pages.reset_password import ResetPasswordPage
 class TestResetPassword(unittest.TestCase):
 
     def test_reset_password(self):
+        #enter any URL or emailid below as input to the test
         reset_page = ResetPasswordPage("https://create.kahoot.it/auth/login", "test@gmail.com")
         reset_page.reset_password()
 
-if __name__ == "__main__":
-    reset_page = ResetPasswordPage("https://google.com", "test@gmail.com")
-    reset_page.reset_password()
